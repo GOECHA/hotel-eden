5b85cfffb8da7b16cca315e952c1a2d65aa97c2b
Chantal Goethals <chantalgoethals@gmail.com>
2022-08-05 19:27:45 -0600

    Feat:finish customer class

--- src/Classes/Customers-class.js
+++ src/Classes/Customers-class.js
@@ -5,11 +5,13 @@ class Customer {
     constructor(customerData) {
       this.id = customerData.id;
       this.name = customerData.name;
+      this.pastBookings = [];
+      this.upcomingBookings = [];
     }
 
 
     // possible functions:
-    // 1, should be able to book a room
+    // 1, should be able to book a room with a selected date
     // 2, should be able to login to account by name and id
     // 3, shoul have a history of visits
     // 4, should have a record of upcoming visits
--- test/Customers-test.js
+++ test/Customers-test.js
@@ -3,8 +3,8 @@ import { expect } from 'chai';
 import Customer from '../src/classes/Customers-class.js';
 const data = require('../src/test-data/all-test-data.js');
 const testCustomerData = data.customerTestData;
-// const testBookingData = data.bookingsTestData;
-// const testRoomData = data.roomTestData;
+const testBookingData = data.bookingsTestData;
+const testRoomData = data.roomTestData;
 
 
 
@@ -12,11 +12,14 @@ const testCustomerData = data.customerTestData;
 describe('Customer', () => {
     let customer1;
     let customer2;
-
+    let customer3;
+    let customer4;
 
     beforeEach(() => {
         customer1 = new Customer(testCustomerData[0]);
         customer2 = new Customer(testCustomerData[1]);
+        customer3 = new Customer(testCustomerData[2]);
+        customer4 = new Customer(testCustomerData[3]);
     });
     it('should be a function', () => {
         expect(Customer).to.be.a('function');
@@ -24,9 +27,23 @@ describe('Customer', () => {
       it('should have an id', () => {
         expect(customer1.id).to.equal(1);
         expect(customer2.id).to.equal(2);
+        expect(customer3.id).to.equal(3);
+        expect(customer4.id).to.equal(4);
       });
       it('should have a name', () => {
         expect(customer1.name).to.equal('Leatha Ullrich');
         expect(customer2.name).to.equal('Rocio Schuster');
+        expect(customer3.name).to.equal('Kelvin Schiller');
+        expect(customer4.name).to.equal('Kennedi Emard');
+      });
+      it.skip('should have a record of pastBookings', () => {
+        expect(customer1.name).to.equal('Leatha Ullrich');
+        expect(customer2.name).to.equal('Rocio Schuster');
+      });
+      it.skip('should have a record of upcomingBookings', () => {
+        expect(customer1.name).to.equal('Leatha Ullrich');
+        expect(customer2.name).to.equal('Rocio Schuster');
       });
 });
+
+//still need to build functions for pastBookings and upcomingBookings
